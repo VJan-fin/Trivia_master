5262a978e5e0da414297ecb438880c23bae741ef
SociaLLabel <aleksandarkuzmanoski11@gmail.com>
2014-04-26 22:07:25 +0200

    MoramMePrimoruvaGit
    Ajde

---
+++ Trivia_master/IUpdatableView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trivia_master
+{
+    interface IUpdatableView
+    {
+        void UpdateView();
+        void CorrectAnswer();
+        void IncorrectAnswer();
+    }
+}
