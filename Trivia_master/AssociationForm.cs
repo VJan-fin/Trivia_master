5d7bab9a8ea8175442fcc3312b5476f8fe506e23
DS19 <dimitarspasovski93@gmail.com>
2014-04-27 18:04:42 +0200

    AssForm

--- Trivia_master/AssociationForm.cs
+++ Trivia_master/AssociationForm.cs
@@ -121,69 +121,35 @@ namespace Trivia_master
 
         private void addAssociations(Graphics gp)
         {
-            Bitmap back = new Bitmap(this.BackgroundImage);
-         
-            
-            var g = Graphics.FromImage(back);
-           
-            
-           g.DrawString("Category : "+category.CategoryName, font, sb, new Rectangle(widthStart, 135, widthSize, heightSize));
+
+            gp.DrawString("Category : " + category.CategoryName, font, sb, new Rectangle(widthStart, 135, widthSize, heightSize));
             for (int i = 0; i < 4; i++)
             {
 
                 Rectangle rec1 = new Rectangle(widthStart, heightStart, widthSize, heightSize);
                 Areas.Add(rec1);
-                g.DrawString((i + 1).ToString() + ". " + question.getQuestion()[i], font, sb, rec1);
+                gp.DrawString((i + 1).ToString() + ". " + question.getQuestion()[i], font, sb, rec1);
                 heightStart += increment;
             }
-           
-            /*int wordCount = question.getCorrectAnswer()[0].Count();
-            
-            int wordCount = question.getCorrectAnswer()[0].Count();
-            StringBuilder strb = new StringBuilder();
-            for (int i = 1; i <= wordCount; i++)
-            {
-                strb.Append("_");
-                if (i != wordCount)
-                  strb.Append(" ");
-            }
-            triviaLabel2.Text = strb.ToString();*/
 
-            Graphics graph = CreateGraphics();
-            /*  Rectangle rec1 = new Rectangle(widthStart, heightStart, widthSize, heightSize);
-              heightStart += increment;
-            g.DrawString((i + 1).ToString() + ". " + question.getQuestion()[i], font, sb, rec1);
-            Rectangle rec2 = new Rectangle(widthStart, heightStart, widthSize, heightSize);
-            heightStart += increment;
-            Rectangle rec3 = new Rectangle(widthStart, heightStart, widthSize, heightSize);
-            heightStart += increment;
-            Rectangle rec4 = new Rectangle(widthStart, heightStart, widthSize, heightSize);
-            g.DrawString("Category : Computer Science", font, sb, new Rectangle(widthStart, 160, widthSize, heightSize));
-            g.DrawString("1. A programming language", font, sb, rec1);
-            g.DrawString("2. John McCarthy", font, sb, rec2);
-            g.DrawString("3. Artificial intelligence", font, sb, rec3);
-            g.DrawString("4. Old as hell", font, sb, rec4);*/
-            BackgroundImage = back;
-            OldPicture = back;
-        }
 
-        /*private void timer1_Tick(object sender, EventArgs e)
-        {
-            TimeLeft--;
-            if (TimeLeft == 0)
-                timer1.Stop();
-            else
+            /*private void timer1_Tick(object sender, EventArgs e)
             {
+                TimeLeft--;
+                if (TimeLeft == 0)
+                    timer1.Stop();
+                else
+                {
              
-                Rectangle rec2 = new Rectangle(500, 300, 40, 40);
-                Bitmap back = new Bitmap(OldPicture);
-               Graphics gp = Graphics.FromImage(back);
-               gp.FillRectangle(new SolidBrush(Color.Transparent), rec2);
-                gp.DrawString(TimeLeft.ToString(), timerFont, sb, rec2);
+                    Rectangle rec2 = new Rectangle(500, 300, 40, 40);
+                    Bitmap back = new Bitmap(OldPicture);
+                   Graphics gp = Graphics.FromImage(back);
+                   gp.FillRectangle(new SolidBrush(Color.Transparent), rec2);
+                    gp.DrawString(TimeLeft.ToString(), timerFont, sb, rec2);
         
-                BackgroundImage = back;
-            }
-        }*/
-    
+                    BackgroundImage = back;
+                }
+            }*/
+        }
     }
 }
