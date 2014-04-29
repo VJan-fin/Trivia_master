a880555d1a7036e74f60ce44096c089b57c077e8
VJan-fin <vik>
2014-04-29 16:25:42 +0200

    v

--- Trivia_master/CloseForm.Designer.cs
+++ Trivia_master/CloseForm.Designer.cs
@@ -33,17 +33,19 @@
             // 
             // CloseForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Trivia_master.Properties.Resources.pozadina;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(375, 215);
+            this.ClientSize = new System.Drawing.Size(500, 265);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "CloseForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Form3";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.CloseForm_Paint);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.CloseForm_MouseClick);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.CloseForm_MouseDown);
