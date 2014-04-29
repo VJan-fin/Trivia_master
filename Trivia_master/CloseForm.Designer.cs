d5a601cf0447f44878841cc32af46eb0b097bafd
DS19 <dimitarspasovski93@gmail.com>
2014-04-29 15:25:18 +0200

    SeNadevamOkejCloseForma

--- Trivia_master/CloseForm.Designer.cs
+++ Trivia_master/CloseForm.Designer.cs
@@ -29,54 +29,21 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CloseForm));
-            this.alphabetButton1 = new Trivia_master.AlphabetButton();
-            this.alphabetButton2 = new Trivia_master.AlphabetButton();
             this.SuspendLayout();
-            // 
-            // alphabetButton1
-            // 
-            this.alphabetButton1.BackColor = System.Drawing.Color.Transparent;
-            this.alphabetButton1.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.alphabetButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.alphabetButton1.Font = new System.Drawing.Font("Forte", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.alphabetButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.alphabetButton1.Location = new System.Drawing.Point(224, 125);
-            this.alphabetButton1.Name = "alphabetButton1";
-            this.alphabetButton1.Size = new System.Drawing.Size(85, 40);
-            this.alphabetButton1.TabIndex = 16;
-            this.alphabetButton1.TabStop = false;
-            this.alphabetButton1.Text = "No";
-            this.alphabetButton1.UseVisualStyleBackColor = true;
-            this.alphabetButton1.Click += new System.EventHandler(this.alphabetButton1_Click);
-            // 
-            // alphabetButton2
-            // 
-            this.alphabetButton2.BackColor = System.Drawing.Color.Transparent;
-            this.alphabetButton2.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.alphabetButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.alphabetButton2.Font = new System.Drawing.Font("Forte", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.alphabetButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.alphabetButton2.Location = new System.Drawing.Point(46, 125);
-            this.alphabetButton2.Name = "alphabetButton2";
-            this.alphabetButton2.Size = new System.Drawing.Size(85, 40);
-            this.alphabetButton2.TabIndex = 15;
-            this.alphabetButton2.TabStop = false;
-            this.alphabetButton2.Text = "Yes";
-            this.alphabetButton2.UseVisualStyleBackColor = true;
-            this.alphabetButton2.Click += new System.EventHandler(this.alphabetButton2_Click);
             // 
             // CloseForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::Trivia_master.Properties.Resources.rsz_1rsz_1rsz_1aaaaq;
-            this.ClientSize = new System.Drawing.Size(370, 205);
-            this.Controls.Add(this.alphabetButton1);
-            this.Controls.Add(this.alphabetButton2);
+            this.BackgroundImage = global::Trivia_master.Properties.Resources.pozadina;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(375, 215);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CloseForm";
             this.Text = "Form3";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.CloseForm_Paint);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.CloseForm_MouseClick);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.CloseForm_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.CloseForm_MouseMove);
@@ -87,7 +54,5 @@
 
         #endregion
 
-        private AlphabetButton alphabetButton2;
-        private AlphabetButton alphabetButton1;
     }
 }
