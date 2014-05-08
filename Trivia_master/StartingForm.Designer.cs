1d8e495c28155dda41d39e729f2fa5202891c944
VJan-fin <vik>
2014-05-08 09:26:49 +0200

    Ситници

--- Trivia_master/StartingForm.Designer.cs
+++ Trivia_master/StartingForm.Designer.cs
@@ -109,7 +109,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.label1.Location = new System.Drawing.Point(328, 574);
+            this.label1.Location = new System.Drawing.Point(322, 574);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(317, 26);
